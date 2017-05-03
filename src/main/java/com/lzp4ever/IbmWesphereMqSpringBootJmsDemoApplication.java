3158c921815f8be6a56e2b2207b621d964108841
luzp <lzp4ever@gmail.com>
2017-05-03 10:53:33 +0800

    init

---
+++ src/main/java/com/lzp4ever/IbmWesphereMqSpringBootJmsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.lzp4ever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmWesphereMqSpringBootJmsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmWesphereMqSpringBootJmsDemoApplication.class, args);
+	}
+}
